a98c92845447f995be2402a676b1ce0536f6b0dc
Jaime Varas Cáceres <jv24040@edu.lu.lv>
2025-04-03 20:38:21 +0300

    added nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Configure path aliases
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+      '@/lib': path.resolve(__dirname, './src/lib'),
+    };
+    return config;
+  },
+  // Other Next.js config options...
 };
 
 export default nextConfig;
