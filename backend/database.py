5dc2dab3eca2a4f440fbc4a5bab90f6a0bc7a7a0
Jaime Varas Cáceres <jv24040@edu.lu.lv>
2025-04-03 19:27:40 +0300

    removed DB password from files, moved to env and changed passwords :)

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = 'postgresql://postgres.gzvnedcfkrdtyvhwphdu:1y0punr6cGoQkB8n@aws-0-eu-central-2.pooler.supabase.com:6543/postgres'
+load_dotenv()  # Looks for .env in current directory by default
+
+# Get DATABASE_URL from environment variables
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
+
 engine = create_engine(DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
